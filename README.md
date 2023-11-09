0effb0beeb144494de180f811e757eed84309642
santiagoa150 <santiago.alvarez@innventasystem.com>
2023-11-09 15:43:27 -0500

    fixing errors

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PPI_SI10
+<h1 align="center">TPPI - SI10</h1>
+
+
+## Descripción
+
+Aplicativo diseñado para recortar, cambiar el contraste y generar un histograma del contraste de imagenes.
+
+## Despliege:
+
+https://ppis10.streamlit.app/
--- main.py
+++ main.py
@@ -1,54 +1,8 @@
-import base64
-import shutil
-import tempfile
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage.io as io
 from skimage import exposure, color
 import streamlit as st
-
-
-def save_temp_image(image_to_save):
-    """
-    Guarda una imagen en un archivo temporal.
-
-    Args:
-        image_to_save (numpy.ndarray): La imagen que se va a guardar.
-
-    Returns:
-        Tuple[str, str]: Una tupla que contiene la ruta del \
-        archivo de imagen temporal y el directorio temporal.
-    """
-
-    # Se crea un directorio temporal para almacenar el archivo de imagen.
-    temp_directory = tempfile.mkdtemp()
-    temp_image_path = temp_directory + "/imagen_recortada.png"
-
-    # Se guarda la imagen en el archivo temporal.
-    io.imsave(temp_image_path, image_to_save)
-    return temp_image_path, temp_directory
-
-
-def image_to_base64(image_to_convert):
-    """
-    Convierte una imagen en formato base64.
-
-    Args:
-        image_to_convert (numpy.ndarray): La imagen que se va a convertir.
-
-    Returns:
-        str: La representación en base64 de la imagen.
-    """
-
-    # Se muestra la imagen en un búfer.
-    image_buffer = io.imshow(image_to_convert, plugin='matplotlib')
-
-    # Se dibuja el búfer para obtener la representación de la imagen.
-    image_buffer.figure.canvas.draw()
-
-    # Se codifica la imagen en base64 y se devuelve como una cadena.
-    return base64.b64encode(image_buffer.figure.canvas.tostring_rgb()).decode()
-
 
 # Se pinta el título de la aplicación.
 st.title("Procesamiento de imágenes")
@@ -80,14 +34,18 @@ if uploaded_image is not None:
         # Se pinta la imagen original.
         st.image(image, caption="Imagen Original", use_column_width=True)
 
-        # Se definen los maximos de los sliders para recortar la imagen
+        # Se obtiene el tamaño de la imagen
         max_x, max_y = image.shape[1], image.shape[0]
 
+        # Se calculan los límites para recortar la imagen
+        new_max_x = int(max_x - max_x * 0.03)
+        new_max_y = int(max_y - max_y * 0.03)
+
         # Se definen los sliders para recortar la imagen
-        crop_x = st.sidebar.slider("Recorte X (inicio) (píxeles)", 0, max_x, 0)
-        crop_y = st.sidebar.slider("Recorte Y (inicio) (píxeles)", 0, max_y, 0)
-        crop_width = st.sidebar.slider("Ancho del Recorte (píxeles)", 0, max_x, max_x)
-        crop_height = st.sidebar.slider("Alto del Recorte (píxeles)", 0, max_y, max_y)
+        crop_x = st.sidebar.slider("Recorte X (inicio) (píxeles)", 0, new_max_x, 0)
+        crop_y = st.sidebar.slider("Recorte Y (inicio) (píxeles)", 0, new_max_y, 0)
+        crop_width = st.sidebar.slider("Ancho del Recorte (píxeles)", 1, new_max_x, new_max_x)
+        crop_height = st.sidebar.slider("Alto del Recorte (píxeles)", 1, new_max_y, new_max_y)
 
         # Se recorta la imagen como un arreglo de numpy
         cropped_image = image[crop_y:crop_y + crop_height, crop_x:crop_x + crop_width]
@@ -115,18 +73,3 @@ if uploaded_image is not None:
         # Se pinta el histograma
         st.pyplot(fig)
 
-
-    # Se define el botón para descargar la imagen recortada
-    if st.sidebar.button("Descargar Imagen Recortada"):
-        # Se guarda temporalmente la imagen
-        path, directory = save_temp_image(cropped_image)
-        with open(path, "rb") as file:
-            image_bytes = file.read()
-            # Se genera el anchor para descargar la imagen
-            st.sidebar.markdown(
-                f'<a href="data:image/png;base64,{base64.b64encode(image_bytes).decode()}" '
-                f'download="imagen_recortada.png">Haga clic aquí para descargar la imagen recortada</a>',
-                unsafe_allow_html=True
-            )
-        # Se limpia el director temporal
-        shutil.rmtree(directory)
